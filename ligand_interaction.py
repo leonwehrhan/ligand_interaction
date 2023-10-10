87ea0f1ca90a675c3ec93034bde5d3bdcbcd22cf
Leon Wehrhan <mail@leonwehrhan.de>
2023-10-10 18:56:11 +0200

    atom, residue class templates

--- ligand_interaction.py
+++ ligand_interaction.py
@@ -70,5 +70,31 @@ class Interface:
             pass
 
 
+class Residue:
+    def __init__(self):
+        self.index = None
+        self.name = None
+        self.resSeq = None
+
+        self.atoms = []
+        self.bonds = []
+
+
+class Atom:
+    def __init__(self):
+        self.index = None
+        self.name = None
+        self.residue = None
+
+        self.bonds = []
+
+        self.is_sidechain = None
+        self.is_hbond_donor = None
+        self.is_hbond_acceptor = None
+        self.is_cation = None
+        self.is_anion = None
+        self.is_hydrophobic = None
+
+
 if __name__ == 'main':
     pass
